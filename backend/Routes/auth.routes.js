4cc549695077ee83959a047f075968401df44162
vish-han <vshlchauhab@gmail.com>
2024-01-27 16:21:40 +0530

    Hosting

--- backend/Routes/auth.routes.js
+++ backend/Routes/auth.routes.js
@@ -4,6 +4,9 @@ const {authenticate} = require("../Utils/jwt");
 
 const Router=express.Router();
 
+Router.get('/',(req,res)=>{
+    res.send("Server is Up and Running");
+});
 Router.post('/signup',signUpController);
 Router.post('/login',loginController);
 
--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,8 @@ const app=express();
 const Router =require('./Routes/auth.routes');
 app.use(express.json());
 connectDB();
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
-app.use('/api',Router);
+
+app.use('/',Router);
 
 app.listen(8000,()=>{
     console.log("Server is running on port 8000");
