00df2e57f51a0575ba754ed678bb4fcbb1faf22d
vish-han <vshlchauhab@gmail.com>
2024-01-27 23:32:33 +0530

    route updted

--- backend/Controllers/case.controller.js
+++ backend/Controllers/case.controller.js
@@ -25,24 +25,42 @@ const addCase = async (req, res) => {
             return res.status(400).json({ message: "Case title and Indiankanoon ID are required" });
         }
 
-        const caseData = new Case({
-            "Case Title": caseTitle,
-            Source: source,
-            "Indiankanoon ID": indiankanoonId,
-            "Case Proceedings": caseProceedings,
-            Citations: citations,
-            "Citations Length": citationsLength,
-            Bench: bench,
-        });
+        // Check if the case already exists based on Indiankanoon ID
+        const existingCase = await Case.findOne({ "Indiankanoon ID": indiankanoonId });
 
+        if (existingCase) {
+            existingCase.set({
+                "Case Title": caseTitle,
+                Source: source,
+                "Indiankanoon ID": indiankanoonId,
+                "Case Proceedings": caseProceedings,
+                Citations: citations,
+                "Citations Length": citationsLength,
+                Bench: bench,
+            });
 
-        await caseData.save();
+            await existingCase.save();
+            res.status(200).json(existingCase);
+        } else {
+            // Case with the given Indiankanoon ID doesn't exist, create a new document
+            const newCase = new Case({
+                "Case Title": caseTitle,
+                Source: source,
+                "Indiankanoon ID": indiankanoonId,
+                "Case Proceedings": caseProceedings,
+                Citations: citations,
+                "Citations Length": citationsLength,
+                Bench: bench,
+            });
 
-        res.status(200).json(caseData);
+            await newCase.save();
+            res.status(201).json(newCase);
+        }
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
 
 module.exports = { getCaseById, addCase };
--- backend/Schema/cases.schema.js
+++ backend/Schema/cases.schema.js
@@ -4,16 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CaseSchema = new Schema({
-
-    "Case Title": {type:String , required:true},
-     Source: {type:String , required:true},
-    "Indiankanoon ID": {type:Number , required:true,unique:true},
-    "Case Proceedings": {type:String },
-    "Citations": {type:Array },
-    "Citations Length": {type:Number },
-     Bench: {type:String },
-});
-
+        "Case Title": String,
+        Source: String,
+        "Indiankanoon ID": Number,
+        "Case Proceedings": String,
+        Citations: [Number],
+        "Citations Length": Number,
+        Bench: String,
+    });
 const Case = mongoose.model('Case', CaseSchema, 'cases');
 
 module.exports = Case;
