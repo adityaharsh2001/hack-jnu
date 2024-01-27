c6ca646fcb488921b400c0b64106c06e6aaf3290
adityaharsh2001 <geekyadi118@gmail.com>
2024-01-27 19:24:35 +0530

    setup cors

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,9 @@ const cookieParser = require('cookie-parser');
     app.use(express.json());
     app.use(cookieParser());
     await connectDB();
-    // app.use(cors({
-    //     origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://dcm.geekyadi.dev'],
-    //     credentials: true,
-    // }));
-
     app.use(cors({
-        origin: "*",
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://dcm.geekyadi.dev'],
         credentials: true,
-        secure: false,
     }));
     app.use('/api', Router);
     app.use('/api/cases', CaseRouter);
