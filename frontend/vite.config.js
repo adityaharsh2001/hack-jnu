c72983ea448354666dac3d826a92c00755ef3227
adityaharsh2001 <geekyadi118@gmail.com>
2024-01-27 19:35:16 +0530

    setup cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+    server: {
+        port: 5173,
+        proxy: {
+        '/api': {
+            target: 'https://dcms.geekyadi.dev/api',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '')
+        }
+        }
+    }
 })
