e7e0b13bc3fcdeb71c1c568567db9412d35fa7e5
adityaharsh2001 <geekyadi118@gmail.com>
2024-01-27 18:02:09 +0530

    setup cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
-const express=require('express');
+const express = require('express');
 const {connectDB} = require("./Utils/db");
-const app=express();
-const Router =require('./Routes/auth.routes');
+const app = express();
+const cors = require('cors');
+const Router = require('./Routes/auth.routes');
 app.use(express.json());
 connectDB();
+app.use(cors({
+    origin: ["http://localhost:3000", "https://dcm.geekyadi.dev"],
+    credentials: true,
+}));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use('/', Router);
 
-app.use('/',Router);
-
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("Server is running on port 8000");
 })
