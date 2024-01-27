85581810928aaed436e219d7a5f23b6e98a27b38
adityaharsh2001 <geekyadi118@gmail.com>
2024-01-27 18:48:07 +0530

    setup cors

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,20 @@ const app = express();
 const Router = require('./Routes/auth.routes');
 const cors = require('cors');
 const CaseRouter = require('./Routes/cases.route');
-app.use(express.json());
 const cookieParser = require('cookie-parser');
-connectDB();
+(async function(){
+    app.use(express.json());
+    app.use(cookieParser());
+    await connectDB();
 
-app.use('/api', Router);
-app.use('/api/cases', CaseRouter);
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://dcm.geekyadi.dev'],
-    credentials: true
-}));
-app.use(express.json());
-connectDB();
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-});
+    app.use('/api', Router);
+    app.use('/api/cases', CaseRouter);
+    app.use(cors({
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://dcm.geekyadi.dev'],
+        credentials: true
+    }));
+    app.use(express.json());
+    app.listen(8000, () => {
+        console.log("Server is running on port 8000");
+    });
+})();
